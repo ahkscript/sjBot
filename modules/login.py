9a90e1b2520c84d9bc38edc5bbd50826cbbe8b4c
Steven James Core <sjc1000@uptonesoftware.net>
2014-07-07 14:55:59 +1000

    Changed to support new location of passwords.
    This will now load a different way. Supporting the move of the password location.

--- modules/login.py
+++ modules/login.py
@@ -10,13 +10,15 @@ else:
 	user 		= self.fullData[0]
 	password 	= self.fullData[1]
 	
-
-	if hashlib.sha224( password ).hexdigest() == self.ownerlist[user.lower()]:
-		if any( self.host == c for c in self.owners ):
-			output 		= "You are already logged in as " + user	
-		else:
-			self.owners.append(self.host)
-			owners 		= self.owners
-			output 		= "Logged in as " + user
-	else:
-		output 		= "Could not login!"
+	for x in self.ownerlist:
+		if x[0] == user.lower():
+			
+			if hashlib.sha224( password ).hexdigest() == x[1].lower():
+				if any( self.host == c for c in self.owners ):
+					output 		= "You are already logged in as " + user	
+				else:
+					self.owners.append(self.host)
+					owners 		= self.owners
+					output 		= "Logged in as " + user
+			else:
+				output 		= "Could not login!"
