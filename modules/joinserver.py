7e3cb58d3b17fea3e864b53b053216a4e31858c4
Steven James Core <sjc1000@uptonesoftware.net>
2014-07-30 18:33:28 +1000

    Delete joinserver.py
    This file is no longer needed. sjBot now joins the server when the class sjBot() is called.

--- modules/joinserver.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-self.irc 		= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-self.irc.connect((self.network, self.port))
-self.irc.send("NICK " + self.botName + " \r\n")
-self.irc.send("USER " + self.botName + " " + self.botName + " " + self.botName + " :Uptone Software\r\n")
-self.irc.send("PRIVMSG NickServ :Identify " + self.botName + " " + self.password + "\r\n")		
-
-output 		= "__notext__"
