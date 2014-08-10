75e9c2d7fd9d36d3b9cd38bbb6f7fd97f3b6d590
Steven James Core <sjc1000@uptonesoftware.net>
2014-08-10 19:46:36 +1000

    Delete hug.py

--- modules/hug.py
+++
@@ -1,9 +0,0 @@
-import random
-
-if len( self.fullData ) > 0:
-	user 		= self.fullData[0]
-else:
-	user 		= self.user
-
-possible 	= ["shhh, there there.", "suck it up, sissy pants.","its okay. &botname is here.", "don't worry bro! I believe at you."] 
-output 		= "\x01ACTION hugs " + user + " and whispers '" + random.choice( possible ) + "'\x01"
