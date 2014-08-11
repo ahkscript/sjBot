89465d52a9e87c11e45dc1cc30daffa767240a9e
Steven James Core <sjc1000@uptonesoftware.net>
2014-08-11 19:39:02 +1000

    Update sjbot.py

--- sjbot.py
+++ sjbot.py
@@ -4,7 +4,7 @@ import commands
 import socket
 import sys
 import time
-
+import imp
 
 print( sys.version )
 print("Bot started. Thank you for using Uptone Software!")
@@ -21,20 +21,32 @@ class sjBot():
 
 	password 	= open('password', 'r').read()
 
-	def __init__(self):
+	def __init__(self, commands):
 		self.irc 		= irc._irc()
 		self.commands 		= commands._commands(self.irc, self.botCmd)
+		print("Commands module imported.")
 		self.irc.joinserver(self.network, self.port, self.botName)
 		self.irc.login(self.password)
-		self.irc.waitUntil("396 " + self.botName)
+		self.irc.waitUntil("396 " + self.botName, 10)
 		self.irc.join( self.channelList )
-		self.loop()
+		self.loop( commands)
 
 
-	def loop(self):
+	def loop(self, commands):
 		recData 	= 1
 
 		while recData:
+
+
+			oldCommands 		= commands
+			try:
+				commands 		= imp.reload( commands )
+				self.commands 		= commands._commands(self.irc, self.botCmd)
+			except:
+				commands 		= oldCommands
+				self.commands		= commands._commands(self.irc, self.botCmd )
+
+
 			recData 	= self.irc.recv(1).replace("\r\n", "")
 			
 
@@ -58,9 +70,13 @@ class sjBot():
 					command 		= splitData[3]
 					command 		= command[2:]
 					params 			= splitData[4:]
+
+					if channel == self.botName:
+						channel 	= user
+
 					commandParams		= [command, user, host, channel, params ]
 					self.commands.callCommand( commandParams )
 
 
 if __name__ == "__main__":
-	sjBot 		= sjBot()
+	sjBot 		= sjBot(commands)
