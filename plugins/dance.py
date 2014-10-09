dab861aaa2c4135d89fd18151d92af98fb15da9f
Steven James Core <sjc1000@uptonesoftware.net>
2014-10-09 12:18:09 +1100

    Create dance.py

---
+++ plugins/dance.py
@@ -0,0 +1,14 @@
+import random
+metaData 	= { "help": ["Dances","Usage: &botcmdance"], "aliases": ["dance", "d"], "owner": 0 }
+
+
+
+def execute(command, user, host, channel, params ):
+	dance  		= ["<(^_^)>",">(^_^)>","<(^_^)<","^(^_^)^","v(^_^)v"]
+	colorCode 	= [ "\x032", "\x033","\x034", "\x035", "\x036", "\x037","\x038", "\x039","\x0310","\x0311","\x0312","\x0313","\x0315" ]
+	returnData 	= []
+
+	for x in range(3, 10):
+		returnData.append( random.choice( colorCode ) + random.choice( dance ) + "\x03" )
+
+	return "Wooo dance time! " + ' '.join( returnData )
