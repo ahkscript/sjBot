5e3101bfabb3a0e25d6adfcce8df41077f0bf1be
Steven James Core <sjc1000@uptonesoftware.net>
2014-10-09 12:25:03 +1100

    Create update.py

---
+++ plugins/update.py
@@ -0,0 +1,5 @@
+metaData 	= {"help": ["This command give a link to the latest maintained versoin of AHK.", "Usage: &botcmdupdate"], "aliases": ["update", "u", "latest", "version"], "owner": 0 }
+
+
+def execute(command, user, host, channel, params ):
+	return "The latest maintained versoin can be found at - http://ahkscript.org/download/ahk-install.exe"
