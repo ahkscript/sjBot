ec9dfd4c4a75facbfdd9966d8d6e1012b0d884d4
Steven James Core <sjc1000@uptonesoftware.net>
2014-10-09 12:24:13 +1100

    Create paste.py

---
+++ plugins/paste.py
@@ -0,0 +1,12 @@
+metaData 	= { "help": ["Tells a user to use a pastebin.","Usage: &botcmdpaste"], "aliases": ["p", "paste"], "owner": 0 }
+
+
+
+
+# url http://ahk.uk.to/
+def execute(command, user, host, channel, params ):
+	if channel == "#ahk":
+		url 	= "http://ahk.us.to/"
+	else:
+		url 		= "http://ahk.uk.to/"
+	return "Please paste your code at the unofficial AutoHotkey pastebin: " + url
