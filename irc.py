3184e555ebbb1021b158059bee053436bb662e28
Steven James Core <sjc1000@uptonesoftware.net>
2014-10-09 12:12:29 +1100

    Update irc.py

--- irc.py
+++ irc.py
@@ -4,9 +4,9 @@ import time
 class _irc():
 	
 	def __init__(self, network, port):
-		print( "IRC module imported." )
-		self.irc 	= socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+		self.irc 		= socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 		self.irc.connect((network, port))
+		self.channelList 	= []
 
 	def joinserver(self, botName ):
 		botName		= bytes( botName, 'utf-8' )
@@ -15,27 +15,12 @@ class _irc():
 		self.irc.send(b"USER " + botName + b" " + botName + b" " + botName + b" :Uptone Software\r\n")
 		return 0
 
-	def waitUntil(self, untilText, timeout ):
-		checkText 		= ""
-		startTime 		= time.time()
-		waitUntil	 	= time.time() + 20
-		print("Waiting for login, or timeout")
 
-		while( 1 ):
-			time.sleep(1)
-			if untilText in checkText or startTime > waitUntil:
-				break
-			checkText 	= self.recv(0)
-			startTime 	= time.time()
-			print( str( startTime ) + " " + str( waitUntil ) )
-		
-
-		print("Done. Joining channels")
+	def nick(self, botName ):
+		self.irc.send(b"NICK " + bytes(botName, 'utf-8' ) + b" \r\n")
 		return 0
 
-
 	def login(self, password):
-		print("Login.")
 		self.irc.send(b"PRIVMSG NickServ :Identify " + self.botName + b" " + password.encode('utf-8') + b"\r\n")
 		return 0
 
@@ -47,29 +32,44 @@ class _irc():
 
 	def join(self, channels ):
 		for k in channels:
+			self.channelList.append( k )
 			self.irc.send(b"JOIN " + bytes( k, 'utf-8' ) + b"\r\n")
 		return 0
 
 
 	def leave(self, channels ):
 		for k in channels:
+			self.channelList.remove( k )
 			self.irc.send(b"PART " + bytes( k, 'utf-8' ) + b"\r\n")
 		return 0
 
 
 	def pMessage(self, toWho, message ):
+		if toWho == "__all__":
+			for x in self.channelList:
+				self.pMessage( x, message )	
+				return 0
 
-		if isinstance(message, str):
-			self.irc.send(b"PRIVMSG " + bytes( toWho, "utf-8") + b" :" + bytes( message, "utf-8") + b"\r\n")
-		else:
-			for k in message:
-				self.irc.send(b"PRIVMSG " + bytes( toWho, "utf-8") + b" :" + bytes( k, "utf-8") + b"\r\n")
-				time.sleep(0.5)
+		if isinstance(toWho, list ):
+			for ch in toWho:
+				if ch in self.channelList:
+					self.pMessage( ch, message )
+
+		try:
+			if isinstance(message, str):
+				self.irc.send(b"PRIVMSG " + bytes( toWho, "utf-8") + b" :" + bytes( message, "utf-8") + b"\r\n")
+			else:
+				for k in message:
+					self.irc.send(b"PRIVMSG " + bytes( toWho, "utf-8") + b" :" + bytes( k, "utf-8") + b"\r\n")
+					time.sleep(0.5)
+		except TypeError:
+			return 1
 		return 0
 
-	def whoIs(self, user ):
-		self.irc.send(b"WHOIS " + bytes( user, "utf-8" ) + b"\r\n")
-		return self.irc.recv(1024)
+	def notice(self, toWho, message ):
+		print( "NOTICE")
+		return 0
+
 	
 	def recv(self, log=0, ammount=1024):
 		try:
