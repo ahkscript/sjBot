7a3f007926d341a1286c17b73fb56c2a50825bf1
Sjc1000 <steven@uptone-software.(none)>
2015-03-11 18:20:38 +1100

    Added wiki command

---
+++ commands/wiki.py
@@ -0,0 +1,23 @@
+import json
+import urllib.parse
+
+meta_data 	= { "help": ["Searches for a wiki page","Usage: &botcmdwiki <query>"], "aliases": ["wiki", "wik"], "owner": 0 }
+
+def execute(parent, commands, irc, user, host, channel, params):
+	if len(params) == 0:
+		return [meta_data['help'][1]]
+	
+	search = 'site:http://en.wikipedia.org%20' + '%20'.join(params)
+	print( search )
+	try:
+		search_data = parent.download_url('https://www.googleapis.com/customsearch/v1?key=AIzaSyAJQbRWt3p4S5sAiHL_iiot87KcbEa0dsQ&cx=009062493091172133168:4ckmchbpuzy&q=' + search)
+	except UnicodeDecodeError:
+		return ['No data found!']
+	
+	response = json.loads(search_data)
+	try:
+		title = response['items'][0]['title']
+		url = response['items'][0]['link']
+	except (IndexError, KeyError):
+		return ['No data found!']
+	return ["\x02" + urllib.parse.unquote( title ) + "\x02 - " + parent.shorten_url(url)]
